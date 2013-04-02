31f79381476fa2007ea997bc5dd54264826b3a48
Yilun Sheng <yilun-sheng@uiowa.edu>
2013-04-02 11:17:59 -0500

    added variable and style sheet

--- itemsdetails.aspx.vb
+++ itemsdetails.aspx.vb
@@ -3,7 +3,13 @@ Partial Class detailsview
     Inherits System.Web.UI.Page
 
     Protected Sub DetailsView1_ItemDeleted(sender As Object, e As System.Web.UI.WebControls.DetailsViewDeletedEventArgs) Handles DetailsView1.ItemDeleted
-        Response.Write("The record has been deleted from the database.")
+        Dim delItem As String = e.Values("pName").ToString()
+        Response.Write("The record ")
+        Response.Write("<span class=deleteditemshighlight>")
+        Response.Write(delItem)
+        Response.Write("</span> has been deleted from the database.")
+
+        Response.AddHeader("REFREASH", "3;URL=./Items.aspx")
     End Sub
 
     Protected Sub DetailsView1_ItemUpdated(sender As Object, e As System.Web.UI.WebControls.DetailsViewUpdatedEventArgs) Handles DetailsView1.ItemUpdated
